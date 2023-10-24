2673f79abff7ca7fb8b556a6ee5bad5a637e4e64
BispoCarlosDev <carlos.oliveirabispo@hotmail.com>
2023-10-24 20:09:22 -0300

    condicao parte 01

---
+++ MODULO D/ex001.js
@@ -0,0 +1,6 @@
+var vel = 30
+console.log(`A Sua velocidade é de ${vel}Km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade máxima permitida. Multado!`)
+}
+console.log(`Use sempre o cinto de Segurança!`)
